c196bb6592c2f06cb81eff938e83e4cbd346a891
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2025-11-11 22:05:04 -0500

    added nativeQuery to use SQL

--- src/main/java/com/example/stardew_almanac/fish/FishRepository.java
+++ src/main/java/com/example/stardew_almanac/fish/FishRepository.java
@@ -11,12 +11,12 @@ import java.util.List;
 public interface FishRepository extends JpaRepository<Fish, String> {
 
     //TODO: Fix queries to use proper JPQL syntax for lists
-    @Query(value = "SELECT f FROM fish WHERE f.seasons LIKE '%:season%'")
+    @Query(value = "SELECT f FROM fish WHERE f.seasons LIKE '%:season%'", nativeQuery = true)
     List<Fish> findBySeason(String season);
 
-    @Query(value = "SELECT f FROM fish WHERE f.locations LIKE '%:location%'")
+    @Query(value = "SELECT f FROM fish WHERE f.locations LIKE '%:location%'", nativeQuery = true)
     List<Fish> findByLocations(String location);
 
-    @Query(value = "SELECT f FROM fish WHERE f.seasons LIKE '%:season%' AND f.locations LIKE '%:location%'")
+    @Query(value = "SELECT f FROM fish WHERE f.seasons LIKE '%:season%' AND f.locations LIKE '%:location%'", nativeQuery = true)
     List<Fish> findByLocationAndSeason(String location, Season season);
 }
