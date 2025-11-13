53c06aacbb88ea137009597d48a900fb8541c1b0
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2025-11-12 22:03:04 -0500

    Update FishRepository.java

--- src/main/java/com/example/stardew_almanac/fish/FishRepository.java
+++ src/main/java/com/example/stardew_almanac/fish/FishRepository.java
@@ -3,6 +3,7 @@ package com.example.stardew_almanac.fish;
 import com.example.stardew_almanac.common.Season;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,15 +12,15 @@ import java.util.Optional;
 @Repository
 public interface FishRepository extends JpaRepository<Fish, String> {
 
-    @Query(value = "SELECT f FROM fish WHERE f.name = :name", nativeQuery = true)
-    Fish findByName(String name);
+    @Query(value = "SELECT * FROM fish WHERE name = :name", nativeQuery = true)
+    Fish findByName(@Param("name") String name);
 
-    @Query(value = "SELECT f FROM fish WHERE f.seasons LIKE '%:season%'", nativeQuery = true)
-    List<Fish> findBySeason(String season);
+    @Query(value = "SELECT * FROM fish WHERE seasons LIKE CONCAT('%', :season, '%')", nativeQuery = true)
+    List<Fish> findBySeason(@Param("season") String season);
 
-    @Query(value = "SELECT f FROM fish WHERE f.locations LIKE '%:location%'", nativeQuery = true)
-    List<Fish> findByLocations(String location);
+    @Query(value = "SELECT * FROM fish WHERE locations LIKE CONCAT('%', :location, '%')", nativeQuery = true)
+    List<Fish> findByLocations(@Param("location") String location);
 
-    @Query(value = "SELECT f FROM fish WHERE f.seasons LIKE '%:season%' AND f.locations LIKE '%:location%'", nativeQuery = true)
-    List<Fish> findByLocationAndSeason(String location, Season season);
+    @Query(value = "SELECT * FROM fish WHERE locations LIKE CONCAT('%', :location, '%') AND seasons LIKE CONCAT('%', :season, '%')", nativeQuery = true)
+    List<Fish> findByLocationAndSeason(@Param("location") String location, @Param("season") String season);
 }
