8b70a2a8961cff441bdfec595a1a5b43008869f4
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2025-12-10 23:07:02 -0500

    Added id values to enum and added getters

--- src/main/java/com/example/stardew_almanac/common/Season.java
+++ src/main/java/com/example/stardew_almanac/common/Season.java
@@ -1,9 +1,31 @@
 package com.example.stardew_almanac.common;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum Season {
-    SPRING,
-    SUMMER,
-    FALL,
-    WINTER,
-    ANY
+    SPRING(1),
+    SUMMER(2),
+    FALL(3),
+    WINTER(4),
+    ANY(5);
+
+    private final int id;
+
+    Season(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private static final Map<Integer, Season> ID_TO_SEASON_MAP =
+            Arrays.stream(values()).collect(Collectors.toMap(Season::getId, s -> s));
+
+    public static Season fromId(int id) {
+        return ID_TO_SEASON_MAP.get(id);
+    }
+
 }
