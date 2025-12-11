d7442d5b0e11a4b4ade021722a2bb6e047fe8a64
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2025-12-10 23:06:39 -0500

    removed fields after updating DB

--- src/main/java/com/example/stardew_almanac/fish/Fish.java
+++ src/main/java/com/example/stardew_almanac/fish/Fish.java
@@ -16,35 +16,24 @@ public class Fish {
     @Id
     private String name;
     private String description;
-    private String time;
     private String weather;
     private int xp;
     private int price;
-    @Convert(converter = SeasonListConverter.class)
-    private List<Season> seasons;
-    @Convert(converter = StringListConverter.class)
-    private List<String> locations;
     @Convert(converter = StringListConverter.class)
     private List<String> usedIn;
 
     public Fish(
             String name,
             String description,
-            String time,
             String weather,
             int xp,
             int price,
-            List<Season> seasons,
-            List<String> locations,
             List<String> usedIn) {
         this.name = name;
         this.description = description;
-        this.time = time;
         this.weather = weather;
         this.xp = xp;
         this.price = price;
-        this.seasons = seasons;
-        this.locations = locations;
         this.usedIn = usedIn;
     }
 
