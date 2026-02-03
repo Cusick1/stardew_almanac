7854c4bcaeea503646f402e2ad492a388dc1e0b4
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2026-02-02 21:55:02 -0500

    Added quality enum

---
+++ src/main/java/com/example/stardew_almanac/common/Quality.java
@@ -0,0 +1,8 @@
+package com.example.stardew_almanac.common;
+
+public enum Quality {
+    NORMAL,
+    SILVER,
+    GOLD,
+    IRIDIUM
+}
--- src/main/java/com/example/stardew_almanac/items/Item.java
+++ src/main/java/com/example/stardew_almanac/items/Item.java
@@ -1,20 +1,23 @@
 package com.example.stardew_almanac.items;
 
+import com.example.stardew_almanac.common.Quality;
+
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
-// @Table(name = "items")
+@Table(name = "items")
 public class Item {
-    // protected int id;
     @Id
     protected String name;
     protected ItemType type;
+    protected Quality quality;
 
-    public Item(String name, ItemType type) {
-        // this.id = id;
+    public Item(String name, ItemType type, Quality quality) {
         this.name = name;
         this.type = type;
+        this.quality = quality;
     }
 
     public Item() {
