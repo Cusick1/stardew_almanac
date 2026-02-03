2affbba2104f435605a4dacef1e35bb03b7d6878
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2026-02-02 21:49:54 -0500

    updated repository to use sql

--- src/main/java/com/example/stardew_almanac/villagers/VillagerRepository.java
+++ src/main/java/com/example/stardew_almanac/villagers/VillagerRepository.java
@@ -1,8 +1,13 @@
 package com.example.stardew_almanac.villagers;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VillagerRepository extends JpaRepository<Villager, String> {
+
+    @Query(value = "SELECT * FROM villagers WHERE name = :name", nativeQuery = true)
+    Villager findByName(@Param("name") String name);
 }
